4573fe210c75b434b0c8584671f0a7b666025779
root <root@WEIKANGWU-PC0>
2024-09-10 21:25:58 +0800

    conv operator

---
+++ my_cuda_conv/my_conv.cu
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+
+
+int main () {
+
+    const int inC = 3;
+    const int inH = 768;
+    const int inW = 512;
+    const int kernelH = 6;
+    const int kernelW = 6;
+    const int outC = 3;
+    const int outH = inH - kernelH + 1;
+    const int outW = inH - kernelW + 1;
+
+    
+
+
+
+
+
+    return 0;
+}
+
+
